cd43884c2ae370c7accd82c8621b9e15e3411849
Gabriel Austria <brhymed01@gmail.com>
2025-03-03 13:49:32 +0800

    Added: categories.styless.scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
-function App() {
+import '../src/categories.styles.scss';
+
+import "./App.css";
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      image: '',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      image: '',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      image: '',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      image: '',
+    },{
+      id: 5,
+      title: 'Mens',
+      image: '',
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories__container">
+      {categories.map((category) => 
+        <div className="category__container">
+        <div className='category__backgroundImage'/>
+        <span className="category__body--container">
+          <h2>{category.title}</h2>
+          <p>Shop Now</p>
+        </span>
+      </div>
+      )}
+      
     </div>
   );
-}
+};
 
 export default App;
