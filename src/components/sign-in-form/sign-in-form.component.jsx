b093211324bcf58ab2132b7df3b2e4e09e999b47
Gabriel Austria <brhymed01@gmail.com>
2025-03-12 16:58:20 +0800

    Updated: Sign-in & Sign-up ==> Authentication

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+
+import "./sign-in-form.styles.scss";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  //   console.log(formFields);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/invalid-credential":
+          alert("Invalid email or password. Please check your credentials.");
+          break;
+        case "auth/user-not-found":
+          alert("User not found. Please check your email.");
+          break;
+        case "auth/wrong-password":
+          alert("Wrong password. Please check your password.");
+          break;
+        case "auth/too-many-requests":
+          alert("Too many requests. Please try again later.");
+          break;
+        default:
+          alert("An unexpected error occured. Please try again");
+      }
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form action="" onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          name="email"
+          id=""
+          required
+          onChange={handleChange}
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          name="password"
+          id=""
+          required
+          onChange={handleChange}
+          value={password}
+        />
+
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type='button' buttonType="google" onClick={signInWithGoogle}>
+            Google Sign In
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
