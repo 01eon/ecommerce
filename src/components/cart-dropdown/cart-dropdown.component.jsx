4d08446e2d515fbed162f64747eb8054bc8d0174
Gabriel Austria <brhymed01@gmail.com>
2025-04-06 17:24:02 +0800

    Refactor: CartDropdown Styled Component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 
 
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, CartItems, CartButton } from './cart-dropdown.styles';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -18,13 +18,13 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
+        <CartDropdownContainer>
+            <CartItems>
                 {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
-            </div>
-            <Button onClick={goToCheckoutHandler}> Go to Checkout </Button>
+            </CartItems>
+            <CartButton as={Button} onClick={goToCheckoutHandler}> Go to Checkout </CartButton>
 
-        </div>
+        </CartDropdownContainer>
     )
 }
 
