14c545559ad10baa11572eaa8a87c541ddd8c1b3
Gabriel Austria <brhymed01@gmail.com>
2025-04-08 17:42:08 +0800

    Refactor: ProductCard Styled Component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 
-import './product-card.styles.scss';
+import { ProductCardContainer, Footer, Name, Price } from './product-card.styles';
 
 import Button from '../button/button.component';
 
@@ -12,15 +12,15 @@ const ProductCard = ({ product }) => {
 
     const addProductToCart = () => addItemToCart(product)
     return (
-        <div className='product-card-container'>
+        <ProductCardContainer>
             <img src={imageUrl} alt={`${name}`} />
-            <div className='footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
+            <Footer>
+                <Name>{name}</Name>
+                <Price>{price}</Price>
+            </Footer>
             <Button buttonType='invert' onClick={addProductToCart}> Add to Cart </Button>
-        </div>
+        </ProductCardContainer>
     )
 }
-
+ 
 export default ProductCard;
