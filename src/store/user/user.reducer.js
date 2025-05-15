6f0159f92c177f45e5a1c4a4705e83cbbf6d165d
Gabriel Austria <brhymed01@gmail.com>
2025-05-15 14:38:15 +0800

    Added: Redux Extended Tools

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = {
 };
 
 export const userReducer = (state=INITIAL_STATE, action) => {
-  console.log("dispatched", action);
+  // console.log("dispatched", action);
   const { type, payload } = action;
 
   switch (type) {
