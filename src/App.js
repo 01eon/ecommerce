eee2fb3e3d2c213ae74fa08578763f287c120097
Gabriel Austria <brhymed01@gmail.com>
2025-06-10 16:10:08 +0800

    Added: Check User Session Saga

--- src/App.js
+++ src/App.js
@@ -2,46 +2,22 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router";
 import { useDispatch } from "react-redux";
 
-
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 
-
-
-import { setCurrentUser } from "./store/user/user.action";
-import {
-  // UserContext
-  createUserDocumentFromAuth,
-  onAuthStateChangedListener,
-} from "./utils/firebase/firebase.utils";
-
-
-
+import { checkUserSession } from "./store/user/user.action";
 
 import "./App.css";
-
-
 
 const App = () => {
   const dispatch = useDispatch();
 
-  // From UserContext
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-    return unsubscribe;
-  }, [dispatch]);
-
-
-
-
+    dispatch(checkUserSession())
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
